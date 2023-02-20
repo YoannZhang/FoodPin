6d4b6d9676699ea859f61a7ba86acc895d16ae31
Yoann Z <zhangyou09@outlook.com>
2023-02-20 17:29:14 +0800

    Chap.7 finished

---
+++ FoodPin/RestaurantListView.swift
@@ -0,0 +1,52 @@
+//
+//  ContentView.swift
+//  FoodPin
+//
+//  Created by Yoann Zhang on 2023/2/20.
+//
+
+import SwiftUI
+
+struct RestaurantListView: View {
+    
+    var restaurantNames = ["Cafe Deadend", "Homei", "Teakha", "Cafe Loisl", "Petite Oyster", "For Kee Restaurant", "Po's Atelier", "Bourke Street Bakery", "Haigh's Choc olate", "Palomino Espresso", "Upstate", "Traif", "Graham Avenue Meats", "Waffle & Wolf", "Five Leaves", "Cafe Lore", "Confessional", "Barrafina", "Donostia", "RoyalOak", "CASK Pub and Kitchen"]
+    
+    var restaurantImages = ["cafedeadend", "homei", "teakha", "cafeloisl", "petiteoyster", "forkee", "posatelier", "bourkestreetbakery", "haigh", "palomino", "upstate", "traif", "graham", "waffleandwolf", "fiveleaves", "cafelore", "confessional", "barrafina", "donostia", "royaloak", "cask"]
+    
+    var restaurantLocations = ["Hong Kong", "Hong Kong", "Hong Kong", "Hong Kong", "Ho ng Kong", "Hong Kong", "Hong Kong", "Sydney", "Sydney", "Sydney", "New York", "New  York", "New York", "New York", "New York", "New York", "New York", "London", "Lon don", "London", "London"]
+    
+    var restaurantTypes = ["Coffee & Tea Shop", "Cafe", "Tea House", "Austrian / Causu al Drink", "French", "Bakery", "Bakery", "Chocolate", "Cafe", "American / Seafood" , "American", "American", "Breakfast & Brunch", "Coffee & Tea", "Coffee & Tea", "L atin American", "Spanish", "Spanish", "Spanish", "British", "Thai"]
+    
+    var body: some View {
+        List {
+            ForEach(restaurantNames.indices, id: \.self) { index in
+                
+                HStack(alignment: .top, spacing: 20) {
+                    Image(restaurantImages[index])
+                        .resizable()
+                        .frame(width: 120, height: 118)
+                        .cornerRadius(20)
+                    VStack(alignment: .leading) {
+                        Text(restaurantNames[index])
+                            .font(.system(.title2, design: .rounded))
+                        Text(restaurantTypes[index])
+                            .font(.system(.body, design: .rounded))
+                        Text(restaurantTypes[index])
+                            .font(.system(.subheadline, design: .rounded))
+                    }
+                }
+            }
+        }
+        .listStyle(.plain)
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        RestaurantListView()
+            .previewDisplayName("Restaurant List View (Light)")
+        RestaurantListView()
+            .preferredColorScheme(.dark)
+            .previewDisplayName("Restaurant List View (Dark)")
+    }
+}
