76f74136f04009fd6410841339f3987da4b0e580
Yoann Z <zhangyou09@outlook.com>
2023-02-20 15:35:08 +0800

    Initial Commit

---
+++ FoodPin/FoodPinApp.swift
@@ -0,0 +1,17 @@
+//
+//  FoodPinApp.swift
+//  FoodPin
+//
+//  Created by Yoann Zhang on 2023/2/20.
+//
+
+import SwiftUI
+
+@main
+struct FoodPinApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
